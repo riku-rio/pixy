743bb08dc88404d6b6d291660b288e90cde4a658
Yosef Ahmed <YosefBesn123@gmail.com>
2026-07-20 10:00:12 +0300

    refactor: separate feature rules from database access

--- src/features/guildFeatureGate.js
+++ src/features/guildFeatureGate.js
@@ -1,14 +1,5 @@
 const { prisma } = require("../config/prisma");
-const { TICKET_ACTIONS } = require("../utils/tickets/actions/ticketActionTypes");
-
-function getDisabledActionCode(settings, action) {
-  if (!settings) return null;
-  if (settings.agentActionsEnabled === false) return "agent_actions_disabled";
-  if (action === TICKET_ACTIONS.CLOSE_TICKET && settings.closeTicketEnabled === false) return "close_ticket_disabled";
-  if (action === TICKET_ACTIONS.RENAME_TICKET && settings.renameReviewEnabled === false) return "rename_review_disabled";
-  if (action === TICKET_ACTIONS.ESCALATE_TICKET && settings.escalationEnabled === false) return "escalation_disabled";
-  return null;
-}
+const { getDisabledActionCode } = require("./guildFeatureRules");
 
 async function getGuildActionRejection(guildId, action) {
   if (!guildId) return "invalid_guild";
@@ -24,4 +15,4 @@ async function getGuildActionRejection(guildId, action) {
   return getDisabledActionCode(settings, action);
 }
 
-module.exports = { getDisabledActionCode, getGuildActionRejection };
+module.exports = { getGuildActionRejection };
